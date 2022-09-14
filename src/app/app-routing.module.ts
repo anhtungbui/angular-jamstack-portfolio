e7747fdc1177485a5885824553da2eaf01df048a
Anh Tung Bui <anhtung.bui@gmail.com>
2022-09-14 14:37:29 +0200

    chore: scaffold prototype with Home & About pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, title: 'Home' },
+  { path: 'about', component: AboutComponent, title: 'About' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
