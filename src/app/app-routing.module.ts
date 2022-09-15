abda9f9f83aa250b9a59fd2a9fa9f4b7012fe46a
Anh Tung Bui <anhtung.bui@gmail.com>
2022-09-16 00:27:13 +0200

    chore: scaffold Home as lazy-loaded module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent, title: 'Home' },
   { path: 'about', component: AboutComponent, title: 'About' },
   { path: 'projects', component: ProjectsComponent, title: 'Projects' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then((m) => m.HomeModule) },
   { path: '**', component: PageNotFoundComponent, title: 'Page Not Found' }
 ];
 
