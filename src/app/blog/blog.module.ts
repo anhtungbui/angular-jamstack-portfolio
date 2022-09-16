90de595017cb4d9b39d99af71df465f70bdee38d
Anh Tung Bui <anhtung.bui@gmail.com>
2022-09-16 21:47:35 +0200

    chore: draft a general layout with common header

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { SharedModule } from '../shared/shared.module';
 import { BlogRoutingModule } from './blog-routing.module';
+
 import { BlogComponent } from './blog.component';
 import { BlogHeaderComponent } from './components/blog-header/blog-header.component';
 import { ArticleListComponent } from './components/article-list/article-list.component';
@@ -9,6 +9,6 @@ import { AuthorComponent } from './components/author/author.component';
 
 @NgModule({
   declarations: [BlogComponent, BlogHeaderComponent, ArticleListComponent, AuthorComponent],
-  imports: [CommonModule, BlogRoutingModule]
+  imports: [SharedModule, BlogRoutingModule]
 })
 export class BlogModule {}
