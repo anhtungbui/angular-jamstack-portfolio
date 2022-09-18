0ca65f16baa2b43fba1402a82051e02ec1314c5b
Anh Tung Bui <anhtung.bui@gmail.com>
2022-09-18 16:54:16 +0200

    feat: add Screenshot modal for ProjectCard

--- src/app/projects/components/project-card/project-card.component.ts
+++ src/app/projects/components/project-card/project-card.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { windowCount } from 'rxjs';
 import { Project } from '../../interfaces/project.interface';
 
 @Component({
@@ -10,9 +9,35 @@ import { Project } from '../../interfaces/project.interface';
 export class ProjectCardComponent {
   @Input() project?: Project;
 
+  modalState = {
+    isVisble: false,
+    title: '',
+    screenshot: '',
+    isSourceCodeAvailable: false,
+    sourceCodeUrl: '',
+    hasLiveDemo: false,
+    liveDemoUrl: ''
+  };
+
   constructor() {}
 
-  onRedirect(url: string) {
+  onRedirect(url: string): void {
     window.open(url, '_blank');
+  }
+
+  onOpenModal(project: Project) {
+    this.modalState = {
+      isVisble: true,
+      title: `${project.title} developed by Anh Tung Bui`,
+      screenshot: project.screenshot,
+      isSourceCodeAvailable: project.isSourceCodeAvailable ? project.isSourceCodeAvailable : false,
+      sourceCodeUrl: project.sourceCodeUrl ? project.sourceCodeUrl : '',
+      hasLiveDemo: project.hasLiveDemo ? project.hasLiveDemo : false,
+      liveDemoUrl: project.liveDemoUrl ? project.liveDemoUrl : ''
+    };
+  }
+
+  handleCancel() {
+    this.modalState.isVisble = false;
   }
 }
--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -3,6 +3,7 @@ import { SharedModule } from '../shared/shared.module';
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzModalModule } from 'ng-zorro-antd/modal';
 
 import { ProjectsComponent } from './projects.component';
 import { ProjectListComponent } from './components/project-list/project-list.component';
@@ -16,6 +17,6 @@ import { ProjectCardComponent } from './components/project-card/project-card.com
     ProjectDetailComponent,
     ProjectCardComponent
   ],
-  imports: [SharedModule, ProjectsRoutingModule, NzCardModule, NzTagModule]
+  imports: [SharedModule, ProjectsRoutingModule, NzCardModule, NzTagModule, NzModalModule]
 })
 export class ProjectsModule {}
