72509833cc314898aeabfb0add9e95421e581796
Anh Tung Bui <anhtung.bui@gmail.com>
2022-09-18 19:10:05 +0200

    chore: remove unused imports

--- src/app/projects/components/project-detail/project-detail.component.ts
+++ src/app/projects/components/project-detail/project-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { combineLatest, filter, map, Observable, switchMap, tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 import { Project } from '../../interfaces/project.interface';
 import { ProjectService } from '../../services/project.service';
 
