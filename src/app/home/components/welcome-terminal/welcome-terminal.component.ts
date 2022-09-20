7e7ecc40118467a4bac428ea952aa0c3682abfda
Anh Tung Bui <anhtung.bui@gmail.com>
2022-09-21 01:21:31 +0200

    chore: attemp to implement Lottie animation

--- src/app/home/components/welcome-terminal/welcome-terminal.component.ts
+++ src/app/home/components/welcome-terminal/welcome-terminal.component.ts
@@ -1,32 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import Typed from 'typed.js';
+import { Component } from '@angular/core';
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'app-welcome-terminal',
   templateUrl: './welcome-terminal.component.html',
   styleUrls: ['./welcome-terminal.component.scss']
 })
-export class WelcomeTerminalComponent implements OnInit {
+export class WelcomeTerminalComponent {
+  options: AnimationOptions = {
+    path: '/assets/json/99312-developer-skills.json'
+  };
+
   constructor() {}
 
-  ngOnInit(): void {
-    const options = {
-      strings: [
-        'Hi there and welcome :)^1000',
-        'My name is Anh Tung',
-        'I love making web apps',
-        'Please check out my projects ^500'
-      ],
-      typeSpeed: 60,
-      backSpeed: 100,
-      backDelay: 500,
-      fadeOut: true,
-      // showCursor: false
-      // cursorChar: '█',
-      cursorChar: '_'
-      // loop: true,
-    };
-
-    new Typed('.text-typed', options);
+  onAnimate(animationItem: AnimationItem): void {
+    console.log(animationItem);
   }
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { NzAutocompleteModule } from 'ng-zorro-antd/auto-complete';
+import { LottieModule } from 'ngx-lottie';
+import player from 'lottie-web';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { WelcomeTerminalComponent } from './components/welcome-terminal/welcome-terminal.component';
 
+export function playerFactory(): any {
+  return player;
+}
+
 @NgModule({
   declarations: [HomeComponent, WelcomeTerminalComponent],
-  imports: [SharedModule, HomeRoutingModule, NzAutocompleteModule]
+  imports: [
+    SharedModule,
+    HomeRoutingModule,
+    NzAutocompleteModule,
+    LottieModule.forRoot({ player: playerFactory })
+  ]
 })
 export class HomeModule {}
