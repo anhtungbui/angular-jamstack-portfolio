53dbece4eb5f99e2ec3b30656e76f6a0d5af1beb
Anh Tung Bui <anhtung.bui@gmail.com>
2022-09-20 23:04:04 +0200

    chore: attempt to fix css for WelcomeTerminal component

--- src/app/home/components/welcome-terminal/welcome-terminal.component.ts
+++ src/app/home/components/welcome-terminal/welcome-terminal.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-welcome-terminal',
   templateUrl: './welcome-terminal.component.html',
   styleUrls: ['./welcome-terminal.component.scss']
 })
-export class WelcomeTerminalComponent {
+export class WelcomeTerminalComponent implements OnInit {
   constructor() {}
+
+  ngOnInit(): void {
+    const options = {
+      strings: [
+        'Hi there and welcome :)^1000',
+        'My name is Anh Tung',
+        'I love making web apps',
+        'Please check out my projects ^500'
+      ],
+      typeSpeed: 60,
+      backSpeed: 100,
+      backDelay: 500,
+      fadeOut: true,
+      // showCursor: false
+      // cursorChar: '█',
+      cursorChar: '_'
+      // loop: true,
+    };
+
+    new Typed('.text-typed', options);
+  }
 }
