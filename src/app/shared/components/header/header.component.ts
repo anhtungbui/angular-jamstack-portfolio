2e6db0fd7bbdd04f21eabb0a31ea9dacf2a5a70a
Anh Tung Bui <anhtung.bui@gmail.com>
2022-09-24 20:32:45 +0200

    feat: highlight active anchors for Header component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-  constructor() {}
+export class HeaderComponent implements OnInit {
+  fragment: string | null = '';
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.fragment.subscribe((fragment) => {
+      this.fragment = fragment;
+    });
+  }
 }
