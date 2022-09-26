19a1a46141b453c870f4ba4ad68d5918a80694bc
Anh Tung Bui <anhtung.bui@gmail.com>
2022-09-26 16:01:32 +0200

    feat: add 2s Loader for Home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject, delay, Observable, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  private unsubscribe$: Subject<void> = new Subject();
+
+  isLoading: boolean = true;
+  private isLoading$: Observable<boolean> = new BehaviorSubject(this.isLoading);
+
   constructor() {}
+
+  ngOnInit(): void {
+    this.isLoading$
+      .pipe(delay(2000), takeUntil(this.unsubscribe$))
+      .subscribe(() => (this.isLoading = false));
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 }
