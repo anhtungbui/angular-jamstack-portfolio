892734746fdb52a81ea72c7a243869d248044f24
Anh Tung Bui <anhtung.bui@gmail.com>
2022-10-11 20:19:37 +0200

    refactor: update content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
-    title: 'Anh Tung Bui - Web Developer'
+    title: 'Anh Tung Bui · Software Developer'
   },
   /* FIXME: */
   { path: '**', redirectTo: '' }
